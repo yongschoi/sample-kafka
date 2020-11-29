0afdb1f5ec3e22be6d6e7f91e67e51de15a8f8a9
yongschoi <yongschoi@naver.com>
2020-11-29 19:40:03 +0900

    Initial commit

---
+++ common/src/main/java/yongs/temp/vo/Member.java
@@ -0,0 +1,22 @@
+package yongs.temp.vo;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.*;
+
+import java.time.LocalDate;
+
+/**
+ * Created by yogschoi@gmail.com on 2020-11-26
+ * Github : http://github.com/yogschoi
+ */
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class Member {
+    private String userid;
+    private String name;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
+    private LocalDate date;
+}
